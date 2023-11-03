9d2a3caa0ece161fc7d83b8f576e0391baa2df3d
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2023-11-03 15:48:08 -0300

    fix error in module declaration

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { PickupCallCardModule } from 'src/app/component/pickup-call-card/pickup-call-card.module';
 import { HomePage } from './home.page';
 
 @NgModule({
@@ -13,8 +14,9 @@ import { HomePage } from './home.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    PickupCallCardModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
 })
 export class HomePageModule {}
