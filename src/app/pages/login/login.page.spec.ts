6b7eacc1da80fcb004846fc80baaedc8c4a0af44
João Pedro Barbosa <info.joaopedrobarbosa@gmail.com>
2023-11-05 11:22:12 -0300

    form validation login

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -3,6 +3,7 @@ import { LoginPage } from './login.page';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('LoginPage', () => {
   let component: LoginPage;
@@ -14,7 +15,8 @@ describe('LoginPage', () => {
       declarations: [LoginPage],
       imports: [
         IonicModule.forRoot(),
-        AppRoutingModule
+        AppRoutingModule, 
+        ReactiveFormsModule
       ]
     }).compileComponents();
 
@@ -24,6 +26,12 @@ describe('LoginPage', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
+
+  it('should create a form on init', () => {
+    component.ngOnInit();
+
+    expect(component.form).not.toBeUndefined();
+  })
 
   it('should go to home page on login', () => {
     spyOn(router, 'navigate');
